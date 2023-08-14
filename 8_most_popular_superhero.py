97bda2e38a6460b5d4368a020f903586c8c072ae
rsk25 <montgomery7289@gmail.com>
2023-08-14 17:01:13 +0900

    refactor 'connection' dataframe: decrease the number of methods in one line

--- 8_most_popular_superhero.py
+++ 8_most_popular_superhero.py
@@ -15,9 +15,13 @@ names = spark.read.schema(schema).option('sep', ' ')\
     .csv('./data/Marvel_Names.txt')
 lines = spark.read.text('./data/Marvel_Graph.txt')
 
-connections = lines.withColumn('id', func.split(func.col('value'), ' ')[0]) \
-    .withColumn('connections', func.size(func.split(func.col('value'), ' ')) - 1) \
-    .groupBy('id').agg(func.sum('connections').alias('connections'))
+tmp_df = lines.withColumn('id', func.split(func.col('value'), ' ')[0])
+tmp_df = tmp_df.withColumn(
+    'connections',
+    func.size(func.split(func.col('value'), ' ')) - 1
+)
+connections = tmp_df.groupBy('id')\
+    .agg(func.sum('connections').alias('connections'))
 
 most_popular = connections.sort(func.col('connections').desc()).first()
 most_popular_name = names.filter(
